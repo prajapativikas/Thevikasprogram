5125475844ccb7b9793f88334f9db9e317c430a4
VIKAS-PC\lenovo <vikasspiritual1993@gmail.com>
2017-03-10 12:11:02 +0530

    practice the java

---
+++ MyProject/src/Banari.java
@@ -0,0 +1,18 @@
+public class Banari
+{
+	public static void main(String[] args) 
+	{
+	int i=0b1010;
+	int j=0B101;
+	int k=0b1010;
+	int K=101;
+	
+	System.out.println(i);
+	System.out.println(j);
+	System.out.println(k);
+	System.out.println(K);
+	
+    }
+
+
+}
