8975c58c8fe18676c352eb630f9dc1c3e41c2f77
VIKAS-PC\lenovo <vikasspiritual1993@gmail.com>
2017-03-10 17:20:33 +0530

    add int M=103

--- MyProject/src/Banari.java
+++ MyProject/src/Banari.java
@@ -7,12 +7,14 @@ public class Banari
 	int k=0b1010;
 	int K=101;
 	int m=102;
+	int M=103;
 	
 	System.out.println(i);
 	System.out.println(j);
 	System.out.println(k);
 	System.out.println(K);
 	System.out.println(m);
+	System.out.println(M);
 	
     }
 
